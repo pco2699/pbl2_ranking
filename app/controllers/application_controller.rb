06abf167679baa0617e9cd11e2dbc764abe17920
pco2699 <aenkun@gmail.com>
2018-04-13 10:24:14 +0900

    Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_filter :basic
+
+  # （略）
+
+  private
+  def basic
+    authenticate_or_request_with_http_basic do |user, pass|
+      user == 'gs' && pass == 'kodama'
+    end
+  end
 end
